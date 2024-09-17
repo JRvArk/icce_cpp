aaf054f028db336c9b4cf43577d414f4ca048de7
JRvArk <47635443+JRvArk@users.noreply.github.com>
2024-09-17 09:01:10 +0200

    q11

---
+++ set2/11/q11.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string inputText;
+    while (getline(cin, inputText));
+        if (inputText.length() <= 72)
+            cout << inputText << '\n';
+        else if (inputText.length() > 72)
+        {
+            
+        }
+        
+}
