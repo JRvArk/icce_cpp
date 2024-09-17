20bc613cd63706883fd92547c62ee2b365528583
TinoAlfie <t.alferink@student.rug.nl>
2024-09-17 11:54:55 +0200

    changes ex4 + (WC) fix tabs + (NC) remove namespace

--- set1/4/rsl.cc
+++ set1/4/rsl.cc
@@ -1,17 +1,14 @@
 #include <iostream>
-									
-namespace {
-char const RSL[] =					// define the RSL identical to the output
+
+char const RSL[] =                  // define the RSL identical to the output
 R"RSL(
 ^\s+Encryption key:(\w+)
 ^\s+Quality=(\d+)
 ^\s+E?SSID:"([[:print:]]+)"
 ^\s+ssid="([[:print:]]+)"
 )RSL";
-}
-
 
 int main()
 {
-    std::cout << RSL << '\n'; // display output to the stream
+    std::cout << RSL << '\n';        // display output to the stream
 }
