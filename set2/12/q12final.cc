a317b11649f732a70e582ece36807ca61b72dec2
TinoAlfie <t.alferink@student.rug.nl>
2024-09-24 15:31:16 +0200

    fix q12

--- set2/12/q12final.cc
+++ set2/12/q12final.cc
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-#define BITS (sizeof(int) * 8)
-
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -18,39 +16,25 @@ int main(int argc, char *argv[])
     string bits;   
     string bitValues;
                                     // Iterate over bit-positions.
-    for (int bitPos = sizeof(int) * CHAR_BIT - 1; bitPos >= 0; --bitPos)
+    for (int bitPos = sizeof(int) * CHAR_BIT - 1; bitPos >= 0; --bitPos)  
     {
         bitValue |= (1 << bitPos);  // Set bit at bitPos to 1.
 
-        if (value & bitValue)       // Check if bit position bitPos of value
-        {                           // is 1.
-            bits.append("1");              
+        if (value & bitValue)       // Check if bit pos. bitPos of value is 1.
+        {
+            bits.append("1");
 
             value -= bitValue;
                                     // Append no "+" if last 1 bit is reached.
             value ? bitValues.append(to_string(bitValue) + " + ") 
                 : bitValues.append(to_string(bitValue));
             
-    }
-    }
-    for(size_t idx = 2; idx != BITS; ++idx)
-    {                               // Only check for 1's as these have values.
-        if ( (binary[idx - 1] == '1') && firstSetBitHandled)
-            cout << " + " << pow(2, BITS - idx);
         } 
-    for(size_t idx = 2; idx != BITS; ++idx)
-    {                               // Only check for 1's as these have values.
-        if ( (binary[idx - 1] == '1') && firstSetBitHandled)
-            cout << " + " << pow(2, BITS - idx);
-        else
-        else
-            if (binary[idx - 1] == '1')
         else 
-            if (binary[idx - 1] == '1')
         {
             bits.append("0");       // Bit position in value is 0.
         }
-        bitValue = 0;               // Reset bit value .
+        bitValue = 0;               // Reset bit value.
     }
     cout << valueCopy << " = " << bits << " = " << bitValues << '\n';
 }
