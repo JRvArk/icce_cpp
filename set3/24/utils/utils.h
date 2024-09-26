8c036b80db1efeeda87f62d6e5fb2c6cee57468b
Spamleg <64894434+Spamleg@users.noreply.github.com>
2024-09-26 02:04:38 +0200

    Add files via upload

---
+++ set3/24/utils/utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+void callRef(std::string const &prog);
+
+void callValue(std::string const &prog);
+
+void fun2(std::string const &str);
+
+void fun(std::string str);
+
+#endif
