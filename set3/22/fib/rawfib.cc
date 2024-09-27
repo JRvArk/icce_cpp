c4a24f59d61b29bbb0cd79e48ed5bc8f36656dbf
Spamleg <64894434+Spamleg@users.noreply.github.com>
2024-09-27 05:22:06 +0200

    Create rawfib.cc

---
+++ set3/22/fib/rawfib.cc
@@ -0,0 +1,8 @@
+#include "fib.ih"
+
+size_t rawfib(size_t value)         // Unoptimized Fibonacci calculation.
+{
+    if (value > ENDFIB) value = ENDFIB;
+    if (value <= 2) return 1;       // Defined values.
+    return rawfib(value - 1) + rawfib(value - 2);
+}
