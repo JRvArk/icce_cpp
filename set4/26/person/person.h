6c6f7d591dced3f46f460529b51226a794829337
TinoAlfie <tinoalferink@gmail.com>
2024-09-29 12:45:54 +0200

    26: header Person

--- set4/26/person/person.h
+++ set4/26/person/person.h
@@ -1,13 +1,26 @@
 #ifndef INCLUDED_PERSON_
 #define INCLUDED_PERSON_
 
+#include <string>
 
 class Person
 {
     public:
-        Person();
+        void setName(std::string const &name);
+        void setAddress(std::string const &address);
+        void setPhone(std::string const &phone);
+        void setMass(size_t mass);
+
+        std::string const &name()    const;
+        std::string const &address() const;
+        std::string const &phone()   const;
+        size_t mass()                const;
 
     private:
+        std::string d_name;         // name of person
+        std::string d_address;      // address field
+        std::string d_phone;        // telephone number
+        size_t      d_mass;         // the mass in kg
 };
         
 #endif
