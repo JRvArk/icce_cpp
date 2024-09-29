4d131cf555ebc5a256cfea555522c8af1ff1ae57
TinoAlfie <tinoalferink@gmail.com>
2024-09-29 14:11:38 +0200

    26: sources for getters and setters

---
+++ set4/26/person/getters_setters/setPhone.cc
@@ -0,0 +1,11 @@
+#include "person.ih"
+
+void Person::setPhone(string const &phone)
+{
+    if(phone.empty())
+        d_phone = "unknown";
+    else if(phone.find_first_not_of("0123456789") == str::npos)
+        d_phone = phone;
+    else
+        cout << "Please provide a phone number only containing digits.\n";
+}
