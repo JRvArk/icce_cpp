4acdf6fda1a7137997156390087be86f4a7ac976
TinoAlfie <t.alferink@student.rug.nl>
2024-09-29 17:32:05 +0200

    add guards

--- set4/27/person/getters_setters/setPhone.cc
+++ set4/27/person/getters_setters/setPhone.cc
@@ -7,6 +7,8 @@ void Person::setPhone(string const &phone)
     else if(phone.find_first_not_of("0123456789") == string::npos)
         d_phone = phone;
     else
+    {
         cout << "Please provide a phone number only containing digits.\n";
         d_phone = "wrong input";
+    }
 }
