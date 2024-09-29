2493e0a86e143fefec3d6a71da40ca90b79e61e9
TinoAlfie <tinoalferink@gmail.com>
2024-09-29 11:35:49 +0200

    25: indentation

--- set4/25/bottle.h
+++ set4/25/bottle.h
@@ -3,16 +3,16 @@
 
 class Bottle
 {
-public:
-    Bottle(double capacity);        
-    double getCapacity();
-    double getAmount();
-    void addWater(double capacity);
-    void removeWater(double amount);
+    public:
+        Bottle(double capacity);
+        double getCapacity();
+        double getAmount();
+        void addWater(double capacity);
+        void removeWater(double amount);
 
-private:
-    double capacity;
-    double amount;
+    private:
+        double capacity;
+        double amount;
 };
 
 #endif
