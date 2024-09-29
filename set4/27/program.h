832ecac9d2e0e9717b1a2a8df1a45ef7a18e4633
TinoAlfie <tinoalferink@gmail.com>
2024-09-29 16:53:25 +0200

    27: add new functions

---
+++ set4/27/program.h
@@ -0,0 +1,11 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+#include <vector>
+
+#include "person/person.h"
+
+void readPersons(vector<Person> &persons);
+void writePersons(const vector<Person> &persons);
+
+#endif
