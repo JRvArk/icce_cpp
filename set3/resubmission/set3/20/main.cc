6b6dedb317275e3a565aae93c7a9a6c784bb64d9
TinoAlfie <t.alferink@student.rug.nl>
2024-10-01 15:02:28 +0200

    20 minor fix

--- set3/resubmission/set3/20/main.cc
+++ set3/resubmission/set3/20/main.cc
@@ -4,10 +4,15 @@
 
 int main(int argc, char *argv[])
 {
-    string const mode = argv[1];
-                                    // First check for correct argument.
-    if (mode.front() == '-' and mode.size() == 2)
-        checkMode(mode.back());
+    if (argc != 2)
+        cout << "Give an argument: -c, -w, or -l.\n";
     else
-        cout << "A wrong argument is given.\n";
+    {
+        string const mode = argv[1];
+                                        // First check for correct argument.
+        if (mode.front() == '-' and mode.size() == 2)
+            checkMode(mode.back());
+        else
+            cout << "A wrong argument is given.\n";
+    }
 }
