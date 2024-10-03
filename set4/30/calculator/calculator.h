6a8ef25388b1df833751f8c59dbb1659ffba363d
TinoAlfie <tinoalferink@gmail.com>
2024-10-03 11:31:37 +0200

    30: add intro, and order + change order of functions in header file to line up with optimal order in order.txt.

--- set4/30/calculator/calculator.h
+++ set4/30/calculator/calculator.h
@@ -23,14 +23,14 @@ class Calculator
     static double s_num2;
     static bool s_int2;
 
-    public:
-        static void run();
-    
     private:
-        static bool const expression();
         static bool const number(double *dest, bool *isInt);
         static bool const getOperator();
+        static bool const expression();
         static void evaluate();
+
+    public:
+        static void run();
 };
         
 #endif
