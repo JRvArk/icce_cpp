369dce395b4d195e6d0fc92a9407ec8b03ef620c
TinoAlfie <tinoalferink@gmail.com>
2024-10-03 20:17:30 +0200

    changed modulus expression check and messages

--- set4/30/calculator/expression.cc
+++ set4/30/calculator/expression.cc
@@ -1,22 +1,32 @@
 #include "calculator.ih"
 
+namespace {                         // test for non-neg ints in mod case.
+    bool expressionModulus(double num1, bool int1, double num2, bool int2)
+    {
+        if ((num1 >= 0 and int1) or (num2 >= 0 and int2))
+        {
+            cout << "When using modulus, ensure both operands are non-negative"
+                 << "integrals.\n";
+            return false;
+        }
+        return true;
+    }
+}
+
 bool const Calculator::expression()
 {
     bool correct = true;
-    if(not number(&s_num1, &s_int1))
+    if (not number(&s_num1, &s_int1))
     {
         cout << "First parameter has to be a number.\n";
         correct = false;
     }
-    if(not getOperator())
+    if (not getOperator())
     {
         cout << "Second parameter has to be an operator.\n";
-        if (s_op == MODULUS)        // Display error message for mod case.
-            cout << "    When using Modulus, both operands have to be non-"
-                 << "negative integers.\n";
         correct = false;
     }
-    if(not number(&s_num2, &s_int2))
+    if (not number(&s_num2, &s_int2))
     {
         cout << "Third parameter has to be a number.\n";
         correct = false;
@@ -25,7 +35,9 @@ bool const Calculator::expression()
     {                               // Number Operator Number.
         cout << "No more than 3 parameters (formatted: num op num) should be "
              << "given.\n";
-        correct = false;
+        return false;               // Immediately return false.
     }
+    if (s_op == MODULUS)
+        correct = expressionModulus(s_num1, s_int1, s_num2, s_int2);
     return correct;
 }
