6a60ecda87c5c043d9581a09a5e4739a09b2dd04
TinoAlfie <43881508+TinoAlfie@users.noreply.github.com>
2024-10-29 13:30:26 +0100

    finish 49

--- Tino/set7/49/main.cc
+++ Tino/set7/49/main.cc
@@ -2,17 +2,25 @@
 #include <ostream>
 #include <ios>
 
+enum {
+    EXPECTED_ARGS = 1               // Num of given arguments expected.
+};
+
 using namespace std;
 
-int main(int argc, char **argv)
+int main(size_t argc, char **argv)
 {
-    ostream out(cout.rdbuf());
+    if (argc != EXPECTED_ARGS)      // Check argc.
+        cout << "Give 1 argument (of type size_t).\n";
 
-    out.setstate(ios::failbit);
+    ostream out(cout.rdbuf());      // Create ostream obj using cout's buffer.
+
+    out.setstate(ios::failbit);     // Set out's state to fail state.
 
     for (size_t idx = 0; idx != stoull(argv[1]); ++idx)
     {
-        if (out.good())
+        if (out.good())             // Check if out is ready for IO.
+                                    // Since out's in failstate, this remains 0.
             out << "Nr. of command line arguments: " << argc << '\n';
     }
 }
