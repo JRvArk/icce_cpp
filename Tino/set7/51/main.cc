bd8a5aa983936c278280075b598c3f08f3eee276
TinoAlfie <43881508+TinoAlfie@users.noreply.github.com>
2024-10-29 15:00:55 +0100

    51

---
+++ Tino/set7/51/main.cc
@@ -0,0 +1,14 @@
+#include "main.ih"
+
+enum
+{
+    EXPECTED_ARGS = 1               // Num of given arguments expected.
+};
+
+int main(int argc, char **argv)
+{
+    if (argc != EXPECTED_ARGS)
+        cout << "Give 1 argument: the name of a student file.\n";
+
+    
+}
---
+++ Tino/set7/51/utility/convert.cc
@@ -0,0 +1,29 @@
+#include "utility.h"
+
+#include <fstream>
+#include <ios>
+
+
+
+void convert(const string &name) {
+                                    // Open file for reading and writing.
+    fstream file{ name, ios::out || ios::in };
+
+    if (!file.is_open()) {          // Check if file is opened.
+        cout << "Failed to open file.\n";
+        return;
+    }
+
+    string output;                  // Will hold (modified) content.
+    string line;
+    while (getline(file, line))
+    {                               // Check if line is email line.
+        if (line.substr(0, 7) == "email: ")
+            line = lowercase(line);
+        output += line + "\n";
+    }
+
+    file.seekp(0);                  // Rewrite file from start.
+    file << output;                 // Replace file content with output.
+    file.close();                   // Close file stream
+}
