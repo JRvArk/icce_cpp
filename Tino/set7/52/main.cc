ec4f91805778c0e8c42812a022ee1c3bd3e492a1
TinoAlfie <43881508+TinoAlfie@users.noreply.github.com>
2024-10-29 15:55:00 +0100

    + comments + header

--- Tino/set7/52/main.cc
+++ Tino/set7/52/main.cc
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <ctime>
+#include <iomanip>
 
 using namespace std;
 
 ostream &now(ostream &os)
 {
-    time_t now = time(nullptr);
+    time_t now = time(nullptr);     // Get current time
 
-    tm* local = localtime(&now);
-
+    tm* local = localtime(&now);    // Convert time_t to tm.
+                                    // Insert time into os in asctime format.
     os << put_time(local, "%a %b %d %H:%M:%S %Y");
 
     return os;
