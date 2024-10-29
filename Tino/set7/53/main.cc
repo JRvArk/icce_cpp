fb639b739e6ef464d3f74cf02bb489bd40ef4a29
TinoAlfie <43881508+TinoAlfie@users.noreply.github.com>
2024-10-29 16:27:16 +0100

    chngs

--- Tino/set7/53/main.cc
+++ Tino/set7/53/main.cc
@@ -7,12 +7,16 @@ int main()
 {
     double value = 12.04;
 
-    cout << "std display         \"" << setw(15) << value << "\"\n"
-         << "left alignedd       \"" << setw(15) << left << value << "\"\n"
-         << "right aligned       \"" << setw(15) << right << value << "\"\n"
-         << "3 digits            \"" << setw(15) << setprecision(3) << fixed 
-         << value << "\"\n"
-         << "4-digit fraction    \"" << setw(15) << setprecision(4) << fixed
-         << value << "\"\n"
-         << "std display         \"" << setw(15) << value << "\"\n";
+    cout << "std display         \"" 
+         << setw(15) << value << "\"\n"
+         << "left alignedd       \"" 
+         << setw(15) << left << value << "\"\n"
+         << "right aligned       \"" 
+         << setw(15) << right << value << "\"\n"
+         << "3 digits            \"" 
+         << setw(15) << setprecision(1) << fixed << value << "\"\n"
+         << "4-digit fraction    \"" 
+         << setw(15) << setprecision(4) << value << "\"\n"
+         << "std display         \"" 
+         << setw(15) << resetioflags(cout.flags()) << value << "\"\n";
 }
